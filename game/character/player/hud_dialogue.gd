e8e0d88980cc897937600fda1d0673ea467afe8c
PhoenixDSteele <phoenixdsteele@gmail.com>
2025-02-28 16:44:32 -0600

    Finished: Dialogue Trigger Area + HUD

--- game/character/player/hud_dialogue.gd
+++ game/character/player/hud_dialogue.gd
@@ -3,3 +3,27 @@ class_name HUD_Dialogue extends Control
 @onready var subtitles: Label = %Subtitles
 @onready var talker_portrait: TextureRect = %TalkerPortrait
 @onready var talker_name: Label = %TalkerName
+@onready var dialogue_audio: AudioStreamPlayer = %DialogueAudio
+@onready var diag_anim: AnimationPlayer = $DiagAnimationPlayer
+@onready var static_loop: AudioStreamPlayer = %StaticLoop
+@onready var comm_start: AudioStreamPlayer = %CommStart
+
+var curent_comm_diag : CommDialogue = null
+
+var comm_active : bool = false
+
+func play_comm_dialogue(comm_diag:CommDialogue):
+	curent_comm_diag = comm_diag
+	self.visible = true
+	static_loop.play()
+	for diag in curent_comm_diag.diag_array:
+		comm_start.play()
+		diag_anim.play("open_diag")
+		talker_name.text = diag.talker
+		subtitles.text = diag.talker + ": " + diag.dialogue
+		talker_portrait.texture = diag.portrait
+		dialogue_audio.stream = diag.audio
+		dialogue_audio.play()
+		await get_tree().create_timer(diag.audio.get_length()).timeout
+	self.visible = false
+	static_loop.stop()
--- game/scenes/levels/debug/trigger_zone.gd
+++ game/scenes/levels/debug/trigger_zone.gd
@@ -1,1 +1,14 @@
 class_name TriggerZone extends Area3D
+
+enum NamedEnum {Dialogue, Event = -1}
+@export var TriggerType: NamedEnum
+@export var trigger_id : int = 0
+
+@export var dialogue : CommDialogue = null
+
+var active : bool = true
+
+func _on_trigger_zone_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		if TriggerType == NamedEnum.Dialogue:
+			body.hud.hud_dialogue.play_comm_dialogue(dialogue)
