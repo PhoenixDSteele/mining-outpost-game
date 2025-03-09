2548380763c8d24c5257fd708a8888c35253d26e
PhoenixDSteele <phoenixdsteele@gmail.com>
2025-03-09 14:01:29 -0500

    Slight Adjustments

--- game/cutscene/scene_info.gd
+++ game/cutscene/scene_info.gd
@@ -3,6 +3,7 @@ class_name SceneInfo extends Node
 @export var actor_positions : Array[Vector3]
 @export var actor_animations : Array[String]
 @export var cam_move : bool = false
+@export var cam_wobble : bool = false
 @export var cam_move_time : float = 1.0
 @export var cam_pos : Vector3
 @export var cam_rot : Vector3
@@ -22,4 +23,4 @@ func set_scene(scene_actors:Array[CutsceneBody]):
 			actor.anim.play(actor_animations[increment])
 		increment += 1
 	
-	return [cam_pos, cam_rot, portait.portraits[emotion], speaker, dialogue, diag_audio, cam_move, cam_move_time]
+	return [cam_pos, cam_rot, portait.portraits[emotion], speaker, dialogue, diag_audio, cam_move, cam_move_time, cam_wobble]
--- game/scenes/levels/main_menu/cutscene_handler.gd
+++ game/scenes/levels/main_menu/cutscene_handler.gd
@@ -9,6 +9,9 @@ signal cutscene_finished
 var actor_list : Array[CutsceneBody] = []
 var scene_list : Array[SceneInfo] = []
 
+var cam_wobble : bool = false
+var wobble_strength : float = 1.2
+
 func _ready() -> void:
 	print(screen_camera.transform)
 	for node in self.get_children():
@@ -23,7 +26,7 @@ func start_cutscene():
 	await get_tree().create_timer(cutscene_delay).timeout
 	cutscene_hud.diag_box.visible = true
 	for scene in scene_list:
-		## Scene Array: [0]cam_pos, [1]cam_rot, [2]portait, [3]name, [4]dialogue, [5]diag_audio, [6]moves? , [7]cam_move_time
+		## Scene Array: [0]cam_pos, [1]cam_rot, [2]portait, [3]name, [4]dialogue, [5]diag_audio, [6]moves? , [7]cam_move_time, [8]cam_wobble?
 		var scene_info:Array = scene.set_scene(actor_list)
 		## portrait:Texture2D, speaker_name: String, dialogue:String, diag_audio:AudioStream
 		cutscene_hud.update_information(scene_info[2], scene_info[3], scene_info[4], scene_info[5])
@@ -36,6 +39,21 @@ func start_cutscene():
 			t.tween_property(screen_camera,"position", scene_info[0], scene_info[6]).set_ease(Tween.EASE_OUT_IN).set_trans(Tween.TRANS_SINE)
 			t.tween_property(screen_camera,"rotation_degrees", scene_info[1], scene_info[6]).set_ease(Tween.EASE_OUT_IN).set_trans(Tween.TRANS_SINE)
 			await t.finished
+		cam_wobble = scene_info[8]
+		if cam_wobble == true:
+			camera_wobble()
+		else:
+			screen_camera.rotation_degrees.x = 0
 		await cutscene_hud.push_cutscene
 	print("cutscene finished")
 	cutscene_finished.emit()
+
+func camera_wobble(wobble_speed:float = 0.2):
+	if cam_wobble == true:
+		print('wobbled')
+		var t = get_tree().create_tween()
+		t.tween_property(screen_camera, "rotation_degrees:x", wobble_strength, wobble_speed)
+		t.tween_callback(func(): wobble_strength *= -1)
+		t.tween_callback(camera_wobble)
+	else:
+		pass
